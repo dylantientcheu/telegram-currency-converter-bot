3f27b667bd96cf7bbad6b99af9de50375ec1033d
Dylan Tientcheu <dylanmambou17@gmail.com>
2018-11-14 02:34:13 +0100

    fix api call

--- api/currency-converter.js
+++ api/currency-converter.js
@@ -4,13 +4,23 @@ const Axios = require("axios"); // Axios library for promisified fetch
 BASE_URL = "https://free.currencyconverterapi.com/api/v6/";
 
 module.exports = {
-  /**
+/**
    * Get the rate exchange
    * @param {*} source
    * @param {*} destination
    */
-  getRate(source, destination) {
+  async getRate(source, destination) {
     query = `${source}_${destination}`;
     return Axios.get(`${BASE_URL}convert?q=${query}&compact=ultra`);
+  },
+
+  async viewCurrencies() {
+    const currencies = Axios.get(`${BASE_URL}currencies`);
+    return currencies;
+  },
+
+  async viewCountries() {
+    const countries = Axios.get(`${BASE_URL}countries`);
+    return countries;
   }
 };
