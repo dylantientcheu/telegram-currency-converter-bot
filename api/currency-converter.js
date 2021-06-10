64fc088c91d813aa04137ea4d7763b3b38d263e1
Dylan Tientcheu <dylantientcheu@gmail.com>
2021-06-10 03:11:35 +0200

    update deps and fix files

--- api/currency-converter.js
+++ api/currency-converter.js
@@ -1,16 +1,19 @@
 // api/currency-converter.js
 
 const Axios = require("axios"); // Axios library for promisified fetch
-BASE_URL = "https://free.currencyconverterapi.com/api/v6/";
+const APIKEY = process.env.API_KEY; // get the Heroku URL for hosting
+BASE_URL = "https://free.currencyconverterapi.com/api/v7/";
 
 module.exports = {
-  /**
-   * Get the rate exchange
-   * @param {*} source
-   * @param {*} destination
-   */
-  getRate(source, destination) {
-    query = `${source}_${destination}`;
-    return Axios.get(`${BASE_URL}convert?q=${query}&compact=ultra`);
-  }
+	/**
+	 * Get the rate exchange
+	 * @param {*} source
+	 * @param {*} destination
+	 */
+	getRate(source, destination) {
+		query = `${source}_${destination}`;
+		return Axios.get(
+			`${BASE_URL}convert?q=${query}&compact=ultra&apiKey=${APIKEY}`
+		);
+	},
 };
